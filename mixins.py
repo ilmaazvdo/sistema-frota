719013fc24abd8dcf03573ae4d33b3361eb989e9
GyanCarlosMateus <56090508+GyanCarlosMateus@users.noreply.github.com>
2025-12-16 15:56:35 -0300

    Classes especiais
    AbastecivelMixin(), ManutenivelMixin()

---
+++ mixins.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from exceptions import ManutencaoInvalidaError
+
+class AbastecivelMixin:
+    def __init__(self):
+        self._historico_abastecimentos = []
+
+    def abastecer(self, litros: float, valor: float, tipo_combustivel: str):
+        registro = {
+            "data": datetime.now().isoformat(),
+            "litros": litros,
+            "valor": valor,
+            "tipo": tipo_combustivel
+        }
+        self._historico_abastecimentos.append(registro)
+        print(f"Abastecimento de {litros}L registrado.")
+
+class ManutenivelMixin:
+    def __init__(self):
+        self._historico_manutencoes = []
+        self._em_manutencao = False
+
+    def registrar_manutencao(self, tipo: str, custo: float, descricao: str):
+        registro = {
+            "data": datetime.now().isoformat(),
+            "tipo": tipo,
+            "custo": custo,
+            "descricao": descricao
+        }
+        self._historico_manutencoes.append(registro)
+        self._em_manutencao = True # Muda estado [cite: 28]
+    
+    def finalizar_manutencao(self):
+        self._em_manutencao = False
