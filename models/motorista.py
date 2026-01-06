73e47c2334de501cf6f0b3f135b930567ab08c93
ilmaazvdo <ilma.azevedo@aluno.ufca.edu.br>
2026-01-06 11:19:04 -0300

    Consolidação da hierarquia Pessoa/Motorista e Validação de CNH
    > - Integração da classe base Pessoa com a especialização Motorista.
    
    Implementação da lógica de tratamento de datas para validade de CNH utilizando o módulo datetime.
    
    Refatoração para garantir que a classe suporte as validações de alocação da frota.

--- models/motorista.py
+++ models/motorista.py
@@ -1,11 +1,16 @@
+from abc import ABC
 from datetime import datetime
 
-class Motorista:
-    def __init__(self, nome, cnh, categoria, validade_cnh):
+class Pessoa(ABC):
+    def __init__(self, nome: str, cpf: str):
         self.nome = nome
-        self.cnh = cnh
-        self.categoria = categoria
-        self.validade_cnh = datetime.strptime(validade_cnh, "%d/%m/%Y")
+        self.cpf = cpf
 
-    def cnh_valida(self):
-        return self.validade_cnh > datetime.now()
+class Motorista(Pessoa):
+    def __init__(self, nome: str, cpf: str, cnh: str, categoria_cnh: str):
+        super().__init__(nome, cpf)
+        self.cnh = cnh
+        self.categoria_cnh = categoria_cnh
+
+    def __repr__(self):
+        return f"Motorista({self.nome}, CNH: {self.categoria_cnh})"
