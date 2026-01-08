36039a85ee590b24abd2f23eca9793007dd4b38d
josenatanielgomes <jose.nataniel@aluno.ufca.edu.br>
2026-01-08 14:03:33 -0300

    commit
    atualizado

--- models/exceptions.py
+++ models/exceptions.py
@@ -1,11 +1,31 @@
 class FrotaError(Exception):
+    """Erro genérico da frota"""
     pass
 
 class ManutencaoInvalidaError(FrotaError):
+    """Erro para manutenção inválida"""
     pass
 
 class AlocacaoInvalidaError(FrotaError):
+    """Erro para alocação inválida de veículo/motorista"""
     pass
 
 class PoliticaNaoAtendidaError(FrotaError):
+    """Erro quando alguma política de uso não é atendida"""
+    pass
+
+class VeiculoIndisponivelError(FrotaError):
+    """Erro quando o veículo não pode ser usado (ex: em manutenção)"""
+    pass
+
+class MotoristaInvalidoError(FrotaError):
+    """Erro quando o motorista não atende aos requisitos"""
+    pass
+
+class VeiculoNaoEncontradoError(FrotaError):
+    """Erro quando o veículo não é encontrado no sistema"""
+    pass
+
+class MotoristaNaoEncontradoError(FrotaError):
+    """Erro quando o motorista não é encontrado no sistema"""
     pass
